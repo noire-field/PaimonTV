f089a798815efca9430684a39f56283f97585a67
Noire Bladefield <noirefield@gmail.com>
2021-06-08 17:37:11 +0700

    Replaced react-native-video with expo-av

--- android/app/src/main/java/com/paimontv/MainApplication.java
+++ android/app/src/main/java/com/paimontv/MainApplication.java
@@ -1,5 +1,7 @@
 package com.paimontv;
 
+import com.paimontv.generated.BasePackageList;
+
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -12,11 +14,15 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.brentvatne.react.ReactVideoPackage;
+import java.util.Arrays;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 
-public class MainApplication extends Application implements ReactApplication {
+import org.unimodules.adapters.react.ModuleRegistryAdapter;
+import org.unimodules.adapters.react.ReactModuleRegistryProvider;
+import org.unimodules.core.interfaces.SingletonModule;
 
+public class MainApplication extends Application implements ReactApplication {
+  private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(new BasePackageList().getPackageList(), null);
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
         @Override
@@ -31,8 +37,12 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
 
-          packages.add(new ReactVideoPackage());
-          
+           // Add unimodules
+          List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
+            new ModuleRegistryAdapter(mModuleRegistryProvider)
+          );
+          packages.addAll(unimodules);
+
           return packages;
         }
 
